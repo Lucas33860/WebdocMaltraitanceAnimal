7b1160546426e7cfff55443f9cdd0e39f957bb9f
Lucas33860 <150592389+Lucas33860@users.noreply.github.com>
2025-01-31 10:25:23 +0100

    audio / prologue  95% end

--- script/image.js
+++ script/image.js
@@ -1,15 +1,38 @@
+const textContainer1 = document.getElementById("text-container1");
+const textContainer2 = document.getElementById("text-container2");
+
+const textContainer3 = document.getElementById("text-container3");
+const textContainer4 = document.getElementById("text-container4");
+const textContainer5 = document.getElementById("text-container5");
+const textContainer6 = document.getElementById("text-container6");
+
+const audio1 = new Audio("../audio/pattemal/Prise1.wav");
+const audio2 = new Audio("../audio/coderural/Prise1.wav");
+
+document.getElementById("next3").onclick = function () {
+  audio1.pause();
+  audio1.currentTime = 0;
+  audio2.pause();
+  audio2.currentTime = 0;
+};
+
+document.getElementById("next4").onclick = function () {
+  audio3.pause();
+  audio3.currentTime = 0;
+  audio4.pause();
+  audio4.currentTime = 0;
+  audio5.pause();
+  audio5.currentTime = 0;
+  audio6.pause();
+  audio6.currentTime = 0;
+};
+
+const audio3 = new Audio("../audio/1976/Prise1.wav");
+const audio4 = new Audio("../audio/2015/Prise1.wav");
+const audio5 = new Audio("../audio/2023/Prise1.wav");
+const audio6 = new Audio("../audio/patte/Prise1.wav");
+
 document.addEventListener("DOMContentLoaded", function () {
-  const textContainer1 = document.getElementById("text-container1");
-  const textContainer2 = document.getElementById("text-container2");
-
-  const textContainer3 = document.getElementById("text-container3");
-  const textContainer4 = document.getElementById("text-container4");
-  const textContainer5 = document.getElementById("text-container5");
-  const textContainer6 = document.getElementById("text-container6");
-
-  const audio1 = new Audio("../audio/Prologue-audio.wav");
-  const audio2 = new Audio("../audio/Prologue-audio.wav");
-
   document.querySelector(".picto1").onclick = function () {
     textContainer1.style.display =
       textContainer1.style.display === "block" ? "none" : "block";
@@ -38,3 +61,83 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   };
 });
+
+document.querySelector(".friseimg1").onclick = function () {
+  textContainer3.style.display =
+    textContainer3.style.display === "block" ? "none" : "block";
+  if (textContainer3.style.display === "block") {
+    audio3.play();
+    audio4.pause();
+    audio5.pause();
+    audio6.pause();
+    audio4.currentTime = 0;
+    audio5.currentTime = 0;
+    audio6.currentTime = 0;
+    textContainer4.style.display = "none";
+    textContainer5.style.display = "none";
+    textContainer6.style.display = "none";
+  } else {
+    audio3.pause();
+    audio3.currentTime = 0;
+  }
+};
+
+document.querySelector(".friseimg2").onclick = function () {
+  textContainer4.style.display =
+    textContainer4.style.display === "block" ? "none" : "block";
+  if (textContainer4.style.display === "block") {
+    audio4.play();
+    audio3.pause();
+    audio5.pause();
+    audio6.pause();
+    audio3.currentTime = 0;
+    audio5.currentTime = 0;
+    audio6.currentTime = 0;
+    textContainer3.style.display = "none";
+    textContainer5.style.display = "none";
+    textContainer6.style.display = "none";
+  } else {
+    audio4.pause();
+    audio4.currentTime = 0;
+  }
+};
+
+document.querySelector(".friseimg3").onclick = function () {
+  textContainer5.style.display =
+    textContainer5.style.display === "block" ? "none" : "block";
+  if (textContainer5.style.display === "block") {
+    audio5.play();
+    audio3.pause();
+    audio4.pause();
+    audio6.pause();
+    audio3.currentTime = 0;
+    audio4.currentTime = 0;
+    audio6.currentTime = 0;
+    textContainer3.style.display = "none";
+    textContainer4.style.display = "none";
+    textContainer6.style.display = "none";
+  } else {
+    audio5.pause();
+    audio5.currentTime = 0;
+  }
+};
+
+document.querySelector(".friseimg4").onclick = function () {
+  textContainer6.style.display =
+    textContainer6.style.display === "block" ? "none" : "block";
+  if (textContainer6.style.display === "block") {
+    audio6.play();
+    audio3.pause();
+    audio4.pause();
+    audio5.pause();
+    audio3.currentTime = 0;
+    audio4.currentTime = 0;
+    audio5.currentTime = 0;
+    textContainer3.style.display = "none";
+    textContainer4.style.display = "none";
+    textContainer5.style.display = "none";
+  } else {
+    audio6.pause();
+    audio6.currentTime = 0;
+  }
+};
--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 document.getElementById("second-page").style.display = "none";
 document.getElementById("third-page").style.display = "none";
 document.getElementById("fourth-page").style.display = "none";
+document.getElementById("fifth-page").style.display = "none";
 
 document.getElementById("start").addEventListener("click", function () {
   document.getElementById("first-page").style.display = "none";
@@ -10,6 +11,9 @@ document.getElementById("start").addEventListener("click", function () {
 document.getElementById("next1").addEventListener("click", function () {
   document.getElementById("second-page").style.display = "none";
   document.getElementById("third-page").style.display = "block";
+  var video = document.querySelector(".video-prologue video");
+  video.pause();
+  video.currentTime = 0;
 });
 
 document.getElementById("next2").addEventListener("click", function () {
@@ -17,11 +21,22 @@ document.getElementById("next2").addEventListener("click", function () {
   document.getElementById("fourth-page").style.display = "block";
 });
 
+document.getElementById("next3").addEventListener("click", function () {
+  document.getElementById("fourth-page").style.display = "none";
+  document.getElementById("fifth-page").style.display = "block";
+});
+
+document.getElementById("next4").addEventListener("click", function () {
+  document.getElementById("fifth-page").style.display = "none";
+  window.location.href = "contents.html";
+});
+
 document.getElementById("leave").addEventListener("click", function () {
   document.getElementById("first-page").style.display = "block";
   document.getElementById("second-page").style.display = "none";
   document.getElementById("third-page").style.display = "none";
   document.getElementById("fourth-page").style.display = "none";
+  document.getElementById("fifth-page").style.display = "none";
 });
 
 document.getElementById("leave2").addEventListener("click", function () {
@@ -29,6 +44,7 @@ document.getElementById("leave2").addEventListener("click", function () {
   document.getElementById("second-page").style.display = "none";
   document.getElementById("third-page").style.display = "none";
   document.getElementById("fourth-page").style.display = "none";
+  document.getElementById("fifth-page").style.display = "none";
 });
 
 document.getElementById("leave3").addEventListener("click", function () {
@@ -36,4 +52,13 @@ document.getElementById("leave3").addEventListener("click", function () {
   document.getElementById("second-page").style.display = "none";
   document.getElementById("third-page").style.display = "none";
   document.getElementById("fourth-page").style.display = "none";
+  document.getElementById("fifth-page").style.display = "none";
+});
+
+document.getElementById("leave4").addEventListener("click", function () {
+  document.getElementById("first-page").style.display = "block";
+  document.getElementById("second-page").style.display = "none";
+  document.getElementById("third-page").style.display = "none";
+  document.getElementById("fourth-page").style.display = "none";
+  document.getElementById("fifth-page").style.display = "none";
 });
