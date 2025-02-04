2c4d3629a8352d0294229818014d1cfb2b794688
Lucas33860 <150592389+Lucas33860@users.noreply.github.com>
2025-02-04 12:46:01 +0100

    ca marche les audios

--- script/contents/chapterancor.js
+++ script/contents/chapterancor.js
@@ -1,59 +1,97 @@
 document.getElementById("chapter1index1").style.display = "none";
+document.getElementById("chapter1index2").style.display = "none";
+document.getElementById("videojulien").style.display = "none";
 
-document.getElementById("chapter1start").addEventListener("click", function () {
+document.getElementById("videodion").style.display = "none";
+
+const addEventListenerIfExists = (id, event, handler) => {
+  const element = document.getElementById(id);
+  if (element) {
+    element.addEventListener(event, handler);
+  }
+};
+
+addEventListenerIfExists("chapter1start", "click", function () {
   document.getElementById("chapter1index1").style.display = "block";
   document.getElementById("contents").style.display = "none";
 });
 
-document.getElementById("chapter2start").addEventListener("click", function () {
+addEventListenerIfExists("chapter2start", "click", function () {
   document.getElementById("chapter2index1").style.display = "block";
   document.getElementById("contents").style.display = "none";
 });
 
-document.getElementById("chapter3start").addEventListener("click", function () {
+addEventListenerIfExists("chapter3start", "click", function () {
   document.getElementById("chapter3index1").style.display = "block";
   document.getElementById("contents").style.display = "none";
 });
 
-document.getElementById("chapter4start").addEventListener("click", function () {
+addEventListenerIfExists("chapter4start", "click", function () {
   document.getElementById("chapter4index1").style.display = "block";
   document.getElementById("contents").style.display = "none";
 });
 
-document.getElementById("creditstart").addEventListener("click", function () {
+addEventListenerIfExists("creditstart", "click", function () {
   document.getElementById("creditindex1").style.display = "block";
   document.getElementById("contents").style.display = "none";
 });
 
-document
-  .getElementById("backchapter1_1")
-  .addEventListener("click", function () {
-    document.getElementById("chapter1index1").style.display = "none";
-    document.getElementById("contents").style.display = "block";
-  });
+addEventListenerIfExists("backchapter1_1", "click", function () {
+  document.getElementById("chapter1index1").style.display = "none";
+  document.getElementById("contents").style.display = "block";
+});
 
-document
-  .getElementById("backchapter2_1")
-  .addEventListener("click", function () {
-    document.getElementById("chapter2index1").style.display = "none";
-    document.getElementById("contents").style.display = "block";
-  });
+addEventListenerIfExists("backchapter1_2", "click", function () {
+  document.getElementById("chapter1index2").style.display = "none";
+  document.getElementById("contents").style.display = "block";
+});
 
-document
-  .getElementById("backchapter3_1")
-  .addEventListener("click", function () {
-    document.getElementById("chapter3index1").style.display = "none";
-    document.getElementById("contents").style.display = "block";
-  });
+addEventListenerIfExists("backchapter1_3", "click", function () {
+  document.getElementById("videodion").style.display = "none";
+  document.getElementById("chapter1index2").style.display = "block";
+});
 
-document
-  .getElementById("backchapter4_1")
-  .addEventListener("click", function () {
-    document.getElementById("chapter4index1").style.display = "none";
-    document.getElementById("contents").style.display = "block";
-  });
+addEventListenerIfExists("backchapter1_4", "click", function () {
+  document.getElementById("videojulien").style.display = "none";
+  document.getElementById("chapter1index2").style.display = "block";
+});
 
-document.getElementById("backcredit1").addEventListener("click", function () {
+addEventListenerIfExists("backchapter2_1", "click", function () {
+  document.getElementById("chapter2index1").style.display = "none";
+  document.getElementById("contents").style.display = "block";
+});
+
+addEventListenerIfExists("backchapter3_1", "click", function () {
+  document.getElementById("chapter3index1").style.display = "none";
+  document.getElementById("contents").style.display = "block";
+});
+
+addEventListenerIfExists("backchapter4_1", "click", function () {
+  document.getElementById("chapter4index1").style.display = "none";
+  document.getElementById("contents").style.display = "block";
+});
+
+addEventListenerIfExists("backcredit1", "click", function () {
   document.getElementById("creditindex1").style.display = "none";
   document.getElementById("contents").style.display = "block";
+});
+
+addEventListenerIfExists("chapter1next1", "click", function () {
+  document.getElementById("chapter1index1").style.display = "none";
+  document.getElementById("chapter1index2").style.display = "block";
+});
+
+addEventListenerIfExists("chapter1next2", "click", function () {
+  document.getElementById("chapter1index2").style.display = "none";
+  document.getElementById("chapter2index1").style.display = "block";
+});
+
+addEventListenerIfExists("dion", "click", function () {
+  document.getElementById("chapter1index2").style.display = "none";
+  document.getElementById("videodion").style.display = "block";
+});
+
+addEventListenerIfExists("julien", "click", function () {
+  document.getElementById("chapter1index2").style.display = "none";
+  document.getElementById("videojulien").style.display = "block";
 });
---
+++ script/contents/dion.js
@@ -0,0 +1,90 @@
+const video = document.querySelector(".video-prologue video");
+const playPauseButton = document.getElementById("play-pause");
+const muteUnmuteButton = document.getElementById("mute-unmute");
+const subtitlesButton = document.getElementById("subtitles");
+const subtitlesMenu = document.getElementById("language-menu");
+const audioTimeline = document.getElementById("audio-timeline");
+const currentTimeDisplay = document.getElementById("current-time");
+const durationTimeDisplay = document.getElementById("duration-time");
+
+playPauseButton.innerHTML =
+  '<img src="/images/play.svg" alt="Play" style="width: 32px; height: 32px; margin-right: 50px;">';
+muteUnmuteButton.innerHTML =
+  '<img src="/images/sound-on.svg" alt="Sound" style="width: 32px; height: 32px;">';
+subtitlesButton.innerHTML =
+  '<img src="/images/subtitles.svg" alt="Subtitles" style="width: 32px; height: 32px;">';
+
+playPauseButton.addEventListener("click", () => {
+  if (video.paused) {
+    video.play();
+    playPauseButton.innerHTML =
+      '<img src="/images/pause.svg" alt="Pause" style="width: 32px; height: 32px; margin-right: 50px;">';
+  } else {
+    video.pause();
+    playPauseButton.innerHTML =
+      '<img src="/images/play.svg" alt="Play" style="width: 32px; height: 32px; margin-right: 50px;">';
+  }
+});
+
+muteUnmuteButton.addEventListener("click", () => {
+  video.muted = !video.muted;
+  muteUnmuteButton.innerHTML = video.muted
+    ? '<img src="/images/sound-off.svg" alt="mute" style="width: 32px; height: 32px;">'
+    : '<img src="/images/sound-on.svg" alt="Unmute" style="width: 32px; height: 32px;">';
+});
+
+subtitlesButton.addEventListener("click", () => {
+  if (subtitlesMenu) {
+    if (!subtitlesMenuVisible) {
+      subtitlesMenu.classList.remove("hidden");
+      subtitlesMenu.style.display = "block";
+      subtitlesMenuVisible = true;
+    } else {
+      subtitlesMenu.classList.toggle("hidden");
+      if (subtitlesMenu.classList.contains("hidden")) {
+        subtitlesMenu.style.display = "none";
+      } else {
+        subtitlesMenu.style.display = "block";
+      }
+    }
+  } else {
+    console.error("Subtitles menu element not found");
+  }
+});
+
+video.addEventListener("loadedmetadata", () => {
+  durationTimeDisplay.textContent = formatTime(video.duration);
+});
+
+video.addEventListener("timeupdate", () => {
+  audioTimeline.value = (video.currentTime / video.duration) * 100;
+  currentTimeDisplay.textContent = formatTime(video.currentTime);
+  durationTimeDisplay.textContent = formatTime(video.duration || 0);
+});
+
+audioTimeline.addEventListener("input", () => {
+  video.currentTime = (audioTimeline.value / 100) * video.duration;
+});
+
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const secs = Math.floor(seconds % 60);
+  return `${minutes}:${secs < 10 ? "0" : ""}${secs}`;
+}
+
+function setSubtitle(trackId) {
+  const video = document.querySelector("video");
+  const tracks = video.textTracks;
+  for (let i = 0; i < tracks.length; i++) {
+    tracks[i].mode = tracks[i].id === trackId ? "showing" : "hidden";
+  }
+}
+
+const back2Button = document.getElementById("leave");
+
+back2Button.addEventListener("click", () => {
+  video.currentTime = 0;
+  video.pause();
+  playPauseButton.innerHTML =
+    '<img src="/images/play.svg" alt="Play" style="width: 32px; height: 32px; margin-right: 50px;">';
+});
